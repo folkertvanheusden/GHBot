27240741b4e051528365feaa75490a813dc98ccc
folkert van heusden <mail@vanheusden.com>
2022-05-15 22:33:05 +0200

    database format

---
+++ ghbot.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `acl_groups` (
+  `group_name` varchar(256) NOT NULL,
+  `who` varchar(256) NOT NULL,
+  PRIMARY KEY (`group_name`,`who`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `acls` (
+  `who` varchar(256) NOT NULL,
+  `command` varchar(256) NOT NULL,
+  PRIMARY KEY (`command`,`who`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `acls` VALUES ('sysops','addacl'),('users','commands'),('sysops','delacl'),('sysops','groupadd'),('sysops','groupdel'),('users','help'),('sysops','listacls'),('sysops','meet')
+
+CREATE TABLE `aliasses` (
+  `command` varchar(256) NOT NULL,
+  `is_command` tinyint(1) DEFAULT 0,
+  `replacement_text` varchar(256) NOT NULL,
+  KEY `command` (`command`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
